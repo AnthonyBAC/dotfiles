02936367fbc9bfda1c8ba22a7df8ce86ca45a7cb
Anthony <anthonyadasme.c@gmail.com>
2025-01-21 20:54:32 -0300

    Todo

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -6,7 +6,7 @@ cp -r ~/.config/gtk-3.0 ~/dotfiles/dotfiles
 cp -r ~/.config/gtk-4.0 ~/dotfiles/dotfiles
 cp -r ~/.config/hypr ~/dotfiles/dotfiles
 cp -r ~/.config/nautilus ~/dotfiles/dotfiles
-cp -r ~/.config/nvim/init.vim ~/dotfiles/dotfiles
+cp -r ~/.config/nvim/init.vim ~/dotfiles/dotfiles/nvim
 cp -r ~/.config/nwg-look ~/dotfiles/dotfiles
 cp -r ~/.config/qt6ct ~/dotfiles/dotfiles
 cp -r ~/.config/rofi ~/dotfiles/dotfiles
@@ -17,7 +17,7 @@ cp -r ~/scripts ~/dotfiles/dotfiles
 
 cd ~/dotfiles/dotfiles
 git add .
-git commit
+git commit -m "Todo"
 git pull
 git push
 
