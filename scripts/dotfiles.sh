4614a793a753f9033954d71b3a0d8ebd9dfb0d4a
Anthony <anthonyadasme.c@gmail.com>
2025-01-21 20:52:49 -0300

    Todo

---
+++ scripts/dotfiles.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/zsh
+cp -r ~/.config/alacritty ~/dotfiles/dotfiles
+cp -r ~/.config/dunst ~/dotfiles/dotfiles
+cp -r ~/.config/fastfetch ~/dotfiles/dotfiles
+cp -r ~/.config/gtk-3.0 ~/dotfiles/dotfiles
+cp -r ~/.config/gtk-4.0 ~/dotfiles/dotfiles
+cp -r ~/.config/hypr ~/dotfiles/dotfiles
+cp -r ~/.config/nautilus ~/dotfiles/dotfiles
+cp -r ~/.config/nvim/init.vim ~/dotfiles/dotfiles
+cp -r ~/.config/nwg-look ~/dotfiles/dotfiles
+cp -r ~/.config/qt6ct ~/dotfiles/dotfiles
+cp -r ~/.config/rofi ~/dotfiles/dotfiles
+cp -r ~/.config/wal ~/dotfiles/dotfiles
+cp -r ~/.config/waybar ~/dotfiles/dotfiles
+cp -f ~/.zshrc ~/dotfiles/dotfiles
+cp -r ~/scripts ~/dotfiles/dotfiles
+
+cd ~/dotfiles/dotfiles
+git add .
+git commit
+git pull
+git push
+
+
+
+
